12d6f38d0631673eed9ecfc1e4f56caf46b956f1
Doug Walter <doug@kosmojo.com>
2014-09-10 17:26:37 -0700

    implented espresso

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+
 });
+
+router.get('/stuff', function(req, res) {
+    res.render('index', { title: 'Expressos', description:'Mocha late something JavaChocScript Framework'});
+});
+
+router.get('/app', function(req, res) {
+    res.render('index', { title: 'Epic Restaurants', description:'At least in my opinion.'});
+});
+
+
+
+
 
 module.exports = router;
